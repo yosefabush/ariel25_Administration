da4162250f3f95a602c4c505323dcbd7cff676fa
Shira Elitzur <Shira Elitzur>
2016-05-26 10:28:46 +0300

    test_and_insertQuestion_updated

---
+++ insertQuestion.php
@@ -0,0 +1,28 @@
+<?php
+/*
+
+*/
+    try{
+        require_once ('db.php');	
+            $bodyQuestion = $_POST['text'];
+            $id = $_POST['gameId'];
+            $imagePath = $_POST['imagePath'];
+            $videoUrl = $_POST['videoUrl'];
+            $answer1 = $_POST['answer1'];
+            $answer2 = $_POST['answer2'];
+            $answer3 = $_POST['answer3'];
+            $answer4 = $_POST['answer4'];
+            $correctAnswer = $_POST['correctAnswer'];
+            
+            $insert  = $db->exec("INSERT INTO questions (Number,GameId,Text,ImagePath,VideoUrl,Answer1,Answer2,Answer3,Answer4,CorrectAnswer) 
+                VALUES (1,$id,'$text','$imagePath','$videoUrl','$answer1','$answer2','$answer3','$answer4',$correctAnswer)");
+            if( $insert !== FALSE ) {
+                echo 1;
+            } else {
+                echo -1;
+            }
+
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    } 
+?>
--- test.js
+++ test.js
@@ -1,7 +1,29 @@
 var insertGameAjax = function() {
    var title = $("#name").val();
    $.get( "insertGame.php?title="+title, function( data ) {
-       //var gameIdFromServer = $.parseJSON(data);
        alert(data);
    }); 
+}
+
+var insertQuestionAjax = function() {    
+    $.ajax( {
+       type: "post",
+        contentType: "application/x-www-form-urlencoded;charset=UTF-8",
+        url:"insertQuestion.php",
+        data: {"gameId":1000,
+                "bodyQuestion":"who is the man?",
+                "imagePath":"image.jpg",
+                "videoUrl":"www.video.com",
+                "answer1":"yossi",
+                "answer2":"moshe",
+                "answer3":"shira",
+                "answer4":"noa",
+                "correctAnswer":3},
+        success: function( data ) {
+            alert(data); 
+        },
+        error: function( data ) {
+            alert( data );
+        }
+    });
 }
